20c65f9b5103b25deb56f981b6e16ccdf463c3b0
Celeste Moore <celestealexmoore@gmail.com>
2021-04-10 18:40:15 -0500

    General Updates

--- Assets/script.js
+++ Assets/script.js
@@ -1,67 +1,159 @@
+var currentQuestionIndex = 0;
+var quizQuestions = document.getElementById("listedQuestions");
+var time = quizQuestions.length * 20;
+var timerID;
 
-/* I need to:
-create a timer for the quiz (2 minutes),
-the time is deducted by 10 when a question is wrong.
-When a question is answered, change to another question.
-when questions are answered or time hits 0, game over.
-When the game is  over, save initials and score.*/
+var quizQuestions = document.getElementById("listedQuestions");
+var timer = document.getElementById("time");
+var quizAnswers = document.getElementById("quizAnswers");
+var submitButton = document.getElementById("submitButton");
+var startButton = document.getElementById("startButton");
+var yourInitials = document.getElementById("yourInitials");
+var autoFeedback = document.getElementById("feedback");
 
+function beginQuiz() {
+    // hide start screen
 
-// Start Button: Once clicked, show questions one at a time 
-// and begin timer. Set timer parameters.
-var h
-
-var startQuiz = $("#startButton");
-var timerID = $("time");
-
-function startQuiz(){
-    //start timer
-timerId = setInterval(clockTick, 1000);
-// show starting time
-timerEl.textContent = time;
-// once timer is going, go get the questions, but the function must be defined.
-getQuestions();
+    var startQuiz = document.getElementById("listedQuestions");
+    startQuiz.setAttribute("class", "hide");
+  
+    // un-hide questions section
+    quizQuestions.removeAttribute("class");
+  
+    // start timer
+    timer = setInterval(timerUpdate, 1000);
+  
+    // show starting time
+    timer.textContent = time; // or timeLeft?
+  
+    showQuestions();
 }
 
-// function getQuestions() {
-//     // get question from other js file.
-//     var 
-// }
+function showQuestions() {
+    // get current question object from array
+    var currentQuestion = listedQuestions[currentQuestionIndex];
+  
+    // update title with current question
+    var questionTitle = document.getElementById("questionTitle");
+    questionTitle.textContent = listedQuestions.title;
+  
+    // clear out any old answer choices
+    quizAnswers.innerHTML = "";
+  
+    // loop over choices
+    currentQuestion.choices.forEach(function(choice, i) {
+      // create new button for each choice
+      var newButton = document.createElement("button");
+      newButton.setAttribute("class", "choice");
+      newButton.setAttribute("value", choice);
+  
+      newButton.textContent = i + 1 + ". " + choice;
+  
+      // attach click event listener to each choice
+      newButton.onclick = questionClick; //why won't this work
+  
+      // display on the page
+      quizAnswers.appendChild(newButton);
+    });
+  }
 
-// startQuiz(function(index, el){
-//     el.addEventListener("click", runQuestions)
-// });
+  function questionClick() {
+    // check if user guessed wrong
+    if (this.value !== listedQuestions[currentQuestionIndex].answer) {
+      // penalize time
+      time -= 10;
+  
+      if (time < 0) {
+        time = 0;
+      }
+  
+      // display new time on page
+      timer.textContent = time;
+      //Give answer feedback
+      autoFeedback.textContent = "Sorry!";
+    } else {
+      autoFeedback.textContent = "Nice Work!";
+    }
+  
+    // flash right/wrong feedback on page for half a second
+    autoFeedback.setAttribute("class", "feedback");
+    setTimeout(function() {
+      autoFeedback.setAttribute("class", "feedback hide");
+    }, 1000);
+  
+    // move to next question
+    currentQuestionIndex++;
+  
+    // check if we've run out of questions
+    if (currentQuestionIndex === questions.length) {
+      endQuiz();
+    } else {
+      showQuestions();
+    }
+  }
+  function endQuiz() {
+    // stop timer
+    clearInterval(timerID);
+  
+    // show end screen
+    var endScreen = document.getElementById("endScreen");
+    endScreen.removeAttribute("class");
+  
+    // show final score
+    var finalScore = document.getElementById("finalScore");
+    finalScore.textContent = time;
+  
+    // hide questions section
+    quizQuestions.setAttribute("class", "hide");
+  }
 
-/* Submit Button: If statement. Once all questions are
-answered or timer reaches 0, end quiz and switch to "All Done."*/
+  function timerUpdate() {
+    // update time
+    time--;
+    timer.textContent = time;
+  
+    // check if user ran out of time
+    if (time <= 0) {
+      endQuiz();
+    }
+  }
 
-// var submitQuiz = $("#submit")
+  function saveScore() {
+    // get value of input box
+    var yourInitials = YourInitials.value.trim();
+  
+    // make sure value wasn't empty
+    if (yourInitials !== "") {
+      // get saved scores from localstorage, or if not any, set to empty array
+      var highscores =
+        JSON.parse(window.localStorage.getItem("highscores")) || [];
+  
+      // format new score object for current user
+      var newestScore = {
+        score: time,
+        yourInitials: yourInitials
+      };
+  
+      // save to localstorage
+      highscores.push(newestScore);
+      window.localStorage.setItem("highscores", JSON.stringify(highscores));
+  
+      // redirect to next page
+      window.location.href = "./highscores.html";
+    }
+  }
 
-// submitQuiz(function(index, el){
-//     el.addEventListener("click", )
-// });
-
-//This a for statement to run the timer. If a question is wrong
-// add ten seconds.
-
-// function timer(event) {
-//     for (var i = 0; i < 18; i++){
-//         /* Created variable i, starting at 9am until 1700 (military time) If i is less than 17, add 1 and go until 17. 
-//         This creates a way for me to get the value stored in the individual ids.*/
-
-//         // using corresponding key in the localStorage for the input id.
-//         console.log(localStorage.getItem(i));
-//         console.log($(`#${i}`))
-//         /*the template literal (backtick) allows you to put javascript directly inside of a string.*/
-//         $(`#${i}`).val(localStorage.getItem(i))
-//     }
-// }
-// storeEvent()
-
-function myFunction() {
-var endScreen = document.getElementById("end-screen");
-if (endScreen.style.display === "inline"){
-    endScreen.style.display === "block";
-}else {
-endScreen.setAttribute("inline");
-}}
+  function enterKey(event) {
+    // "13" represents the enter key
+    if (event.key === "Enter") {
+      saveScore();
+    }
+  }
+  
+  // user clicks button to submit initials
+  submitButton.onclick = saveScore;
+  
+  // user clicks button to start quiz
+  startButton.onclick = beginQuiz;
+  
+  yourInitials.onkeyup = enterKey;
